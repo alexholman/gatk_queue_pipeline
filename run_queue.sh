8fd97cd7ead6cf891a18b3666e4b6244d4ef22c9
alexholman <aholman@jimmy.harvard.edu>
2015-05-13 12:02:23 -0400

    Updated flags to include DBs for VQSR

--- run_queue.sh
+++ run_queue.sh
@@ -7,9 +7,16 @@ java -jar $APPS_PATH/Queue_current/Queue.jar \
   --reference $DB_PATH/gatk/hg19/ucsc.hg19.fasta \
   -L two_region_list.list \
   --input input_list.txt \
+  --hapmap $DB_PATH/gatk/hg19/hapmap_3.3.hg19.vcf \
+  --omni $DB_PATH/gatk/hg19/1000G_omni2.5.hg19.vcf \
+  --thousandGenomes $DB_PATH/gatk/hg19/1000G_phase1.snps.high_confidence.hg19.vcf \
+  --mills $DB_PATH/gatk/hg19/Mills_and_1000G_gold_standard.indels.hg19.vcf \
+  --dbsnpvqsr $DB_PATH/gatk/hg19/dbsnp_137.hg19.vcf \
+  --graphviz graph_out.gv \
+  --graphviz_scatter_gather sg_graph_out.gv \
+  --scatter_gather 10 \
   -jobRunner GridEngine \
   -startFromScratch \
   -run
 
 
-#  --input data/M_2915_1.contigSort.coordSort.bam \
