fd03ba4c981d938cf7fc0599d7aef4df6d97f906
Alexander Holman <ah654@eris1n2.research.partners.org>
2015-06-30 11:47:43 -0400

    Removed MQ from both SNP and INDEL recal. restored other metrics. added in --maxGaussians 4 to indel

--- ExomeGATKPipeline.scala
+++ ExomeGATKPipeline.scala
@@ -570,8 +570,7 @@ class DataProcessingPipeline extends QScript {
     this.resource :+= new TaggedFile(omni, "known=false,training=true,truth=true,prior=12.0")
     this.resource :+= new TaggedFile(thousandGenomes, "known=false,training=true,truth=false,prior=10.0")
     this.resource :+= new TaggedFile(dbSNPvqsr, "known=true,training=false,truth=false,prior=2.0")
-    this.use_annotation ++= List("QD", "ReadPosRankSum", "FS", "InbreedingCoeff")
-
+    this.use_annotation ++= List("QD", "MQRankSum", "ReadPosRankSum", "FS", "InbreedingCoeff")
     if (!qscript.intervalString.isEmpty) this.intervalsString ++= Seq(qscript.intervalString)
     else if (qscript.intervals != null) this.intervals :+= qscript.intervals
 
@@ -592,7 +591,8 @@ class DataProcessingPipeline extends QScript {
     this.tranche ++= List("100.0", "99.9", "99.0", "90.0")
     this.resource :+= new TaggedFile(mills, "known=false,training=true,truth=true,prior=12.0")
     this.resource :+= new TaggedFile(dbSNPvqsr, "known=true,training=false,truth=false,prior=2.0")
-	this.use_annotation ++= List("QD", "DP", "FS", "ReadPosRankSum", "InbreedingCoeff")
+    this.use_annotation ++= List("QD", "ReadPosRankSum", "FS", "InbreedingCoeff")
+    this.maxGaussians = 4
 
     if (!qscript.intervalString.isEmpty) this.intervalsString ++= Seq(qscript.intervalString)
     else if (qscript.intervals != null) this.intervals :+= qscript.intervals
